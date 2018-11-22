64c74b873fc9cd93947ce46bf216b6e59ee2c62d
Adalia <haihui1215@163.com>
2018-11-22 13:17:31 +0800

    add compare tow set

--- src/test/java/com/learn/lhh/CompareOldAndNew.java
+++ src/test/java/com/learn/lhh/CompareOldAndNew.java
@@ -8,24 +8,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.learn.lhh.Common.ParseFile;
 
 public class CompareOldAndNew {
-    public static Map<String, String> compare(String object, JSONArray oldDataList, JSONArray newDataList) {
-        Map<String, String> result = new HashMap<String, String>();
-
-        if (oldDataList == null) {
-            result.put("error", "true");
-            result.put("msg", "旧接口返回的数据为0，无需比较！");
-            return result;
-        }
-        if (newDataList == null) {
-            result.put("error", "true");
-            result.put("msg", "新接口返回的数据为0，无需比较！");
-            return result;
-        }
-        if (!(oldDataList.size() == newDataList.size())) {
-            result.put("error", "true");
-            result.put("msg", "两个接口的返回的数据数量不相等！");
-            return result;
-        }
+    public static Map<String, Object> compare(String object, JSONArray oldDataList, JSONArray newDataList) {
+        Map<String, Object> result = new HashMap<String, Object>();
 
         TreeMap<String, Map> oldDataMap = new TreeMap<String, Map>();
         TreeMap<String, Map> newDataMap = new TreeMap<String, Map>();
@@ -35,24 +19,46 @@ public class CompareOldAndNew {
             Map oneData = (Map) oldDataList.get(i);
             String okey = (String) oneData.get("ContactID");
             oldDataMap.put(okey, oneData);
-            //System.out.println("=============qqqqqqq");
         }
+        //将新接口返回的数据list转换为<数据id,详情>的map
         for (int i = 0; i < newDataList.size(); i++) {
             Map oneData = (Map) newDataList.get(i);
             String nkey = (String) oneData.get("_id");
             newDataMap.put(nkey, oneData);
-           // System.out.println("---------------wwwwwwww");
         }
-        Set oldDataIds = oldDataMap.keySet();
-        Set newDataIds = newDataMap.keySet();
-        Iterator ite1 = oldDataIds.iterator();
+        //获取新旧接口所有的数据ID
+        HashSet oldDataIds = new HashSet(oldDataMap.keySet());
+        HashSet newDataIds = new HashSet(newDataMap.keySet());
 
-        if(!newDataIds.containsAll(oldDataIds)){
+        //找到新旧接口的数据ID的交集，以及旧接口返回新接口没返回的数据，以及新接口返回旧接口没返回的数据id
+        Map<Integer, HashSet<String>> findDiff = findListDiff(oldDataIds,newDataIds);
+        HashSet oldOnly = new HashSet<String>(findDiff.get(0));
+        HashSet newOnly = new HashSet<String>(findDiff.get(2));
+        HashSet bothDataIds = new HashSet<String>(findDiff.get(1));
+        System.out.println("===*****==="+findDiff);
+        if (oldDataList == null) {
             result.put("error", "true");
-            result.put("msg", "两个接口的返回的数据ID不相同！");
+            result.put("msg", "旧接口返回的数据为0，无需比较！");
+            result.put("result",findDiff);
             return result;
         }
-        //比较所有数据的各字段值
+        if (newDataList == null) {
+            result.put("error", "true");
+            result.put("msg", "新接口返回的数据为0，无需比较！");
+            result.put("result",findDiff);
+            return result;
+        }
+        if (!(oldDataList.size() == newDataList.size())||!newDataIds.containsAll(oldDataIds)) {
+            result.put("error", "true");
+            result.put("msg", "两个接口的返回的数据不相同！");
+            result.put("result",findDiff);
+            return result;
+        }
+
+        Iterator ite1 = bothDataIds.iterator();
+
+
+        //比较两个接口所有数据的各字段值
         int count = 1;
         while(ite1.hasNext()){//每条数据遍历
             System.out.println("开始处理第"+count+"条数据！");
@@ -100,6 +106,40 @@ public class CompareOldAndNew {
 
         return result;
     }
+    /**
+     * 获取两个集合不同
+     * @param rps1  rps1数据
+     * @param rps2  rps2数据
+     * @return  0:rps1中独有的数据;1:交集的数据;2:rps2中的独有数据
+     */
+    private static Map<Integer, HashSet<String>> findListDiff(HashSet rps1,HashSet rps2){
+        //判断不能为空
+        if(rps1 == null || rps1.isEmpty() || rps2 == null || rps1.isEmpty()) return null;
 
+        //保存最后的数据
+        Map<Integer, HashSet<String>>  mapList = new HashMap<Integer, HashSet<String>>(3);
+
+        //复制rps1，作为备份
+        HashSet<String> rps1_bak = new HashSet<String>(rps1);
+
+        //1、获取rps1中与rps2中不同的元素
+        rps1.removeAll(rps2);
+
+        //2、获取rps1和rps2中相同的元素
+        rps1_bak.removeAll(rps1);
+
+        //3、获取rps2中与rps1中不同的元素
+        rps2.removeAll(rps1_bak);
+
+        //经过此转换后rps1中数据与rps2中的数据完全不同
+        //rps1_bak是rps1和rps2的交集
+        //rps2中的数据与rps1中的数据完全不同
+
+        mapList.put(0, rps1);//rps1中独有的数据
+        mapList.put(1, rps1_bak);//交集的数据
+        mapList.put(2, rps2);//rps2中的独有数据
+
+        return mapList;
+    }
 }
 
